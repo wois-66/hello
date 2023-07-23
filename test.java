bb4eef01f97ee886a04aae431b92c6c616ef5ed6
fada <hello@itcast.cn>
2023-07-23 22:01:02 +0800

    第二次测试

---
+++ test.java
@@ -0,0 +1,13 @@
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("你好");
+        System.out.println("你几号");
+        System.out.println("11111111");
+
+
+
+
+
+    }
+}
